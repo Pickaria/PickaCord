b0e9af7b4b1d7b759fc563e5c3b7809c7143692f
Erwan <erwan.legloannec@outlook.com>
2022-12-25 19:11:19 +0100

    Update bot status

--- src/main/kotlin/fr/pickaria/bot/Main.kt
+++ src/main/kotlin/fr/pickaria/bot/Main.kt
@@ -2,7 +2,6 @@ package fr.pickaria.bot
 
 import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.utils.env
-import fr.pickaria.bot.extensions.PingExtension
 import fr.pickaria.bot.extensions.IpExtension
 
 private val TOKEN = env("TOKEN")
@@ -10,8 +9,12 @@ private val TOKEN = env("TOKEN")
 suspend fun main() {
     val bot = ExtensibleBot(TOKEN) {
         extensions {
-            add(::PingExtension)
+            // add(::PingExtension)
             add(::IpExtension)
+        }
+
+        presence {
+            playing("Pickaria version alpha")
         }
     }
 
